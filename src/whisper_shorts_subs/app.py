c4fda993ca2359ec6792056cd42fe36d5b0b6fef
Ryan Baten <rbb.baten@gmail.com>
2023-12-05 00:01:08 -0700

    sliders for customization options, highlighting current word

--- src/whisper_shorts_subs/app.py
+++ src/whisper_shorts_subs/app.py
@@ -1,14 +1,18 @@
 import customtkinter
-from tkinter import CENTER, filedialog
+from tkinter import CENTER, filedialog, Canvas, PhotoImage
 from faster_whisper import WhisperModel
 
 import threading
 import queue
 import tempfile
+import numpy as np
+import cv2
 import os
 
+from PIL import Image, ImageTk
+
 from .audio import add_movie_audio
-from .subtitle import create_segments, create_subtitled_video
+from .subtitle import create_segments, create_subtitled_video, add_text_with_outlines, add_words_with_outlines
 from .transcribe import transcribe_with_timestamps
 from .util import string_to_words, words_to_string
 
@@ -23,38 +27,145 @@ class App(customtkinter.CTk):
         self,
         model_size="small",
         model_kwargs=None,
-        transcribe_kwargs=None,
-        subtitle_kwargs=None
+        canvas_width=180,
+        canvas_height=320,
+        words_per_segment=5,
+        font_scale=2,
+        outline_scale=8,
+        orient_y_percent=0.5
     ):
         super().__init__()
         self.model_kwargs = model_kwargs if model_kwargs is not None else {"device": "cpu", "compute_type": "int8"}
-        self.transcribe_kwargs = transcribe_kwargs if transcribe_kwargs is not None else {}
-        self.subtitle_kwargs = subtitle_kwargs if subtitle_kwargs is not None else {}
         self.model = WhisperModel(model_size, **self.model_kwargs)
         self.transcription_queue = queue.Queue()
+        self.export_queue = queue.Queue()
         self.input_video = ""
+        self.font_scale = font_scale
+        self.orient_y_percent = orient_y_percent
+        self.words_per_segment = words_per_segment
+        self.outline_scale = outline_scale
+        self.canvas_width = canvas_width
+        self.canvas_height = canvas_height
+        self.canvas_image = None
 
         self.title("whisper-shorts-subs")
-        self.geometry(f"{1000}x{300}")
+        self.geometry(f"{1024}x{512}")
 
-        self.grid_rowconfigure(6, weight=1)
+        self.grid_rowconfigure(14, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.textbox = customtkinter.CTkTextbox(self)
-        self.textbox.grid(column=0, row=0, rowspan=7, sticky="nsew", padx=5, pady=5)
+        self.textbox.grid(column=0, row=0, rowspan=15, sticky="nsew", padx=5, pady=5)
 
         self.button_load_video = customtkinter.CTkButton(self, text="Transcribe Video", command=self.transcribe_video)
-        self.button_load_video.grid(column=1, row=0, sticky="n", padx=5, pady=5)
-        self.button_export_video = customtkinter.CTkButton(self, text="Export Video", command=self.export_video)
-        self.button_export_video.grid(column=1, row=1, sticky="n", padx=5, pady=5)
-
+        self.button_load_video.grid(column=1, row=0, padx=5, pady=24, columnspan=2)
         width = self.button_load_video.winfo_reqwidth()
+
+        self.y_slider_label = customtkinter.CTkLabel(self, text="Y:")
+        self.y_slider_label.grid(column=1, row=1)
+        self.y_slider = customtkinter.CTkSlider(
+            self,
+            from_=0,
+            to=0.8,
+            number_of_steps=16,
+            width=width,
+            command=lambda x: self.set_and_update_canvas("orient_y_percent", x)
+        )
+        self.y_slider.set(0.5)
+        self.y_slider.grid(column=2, row=1)
+
+        self.font_scale_slider_label = customtkinter.CTkLabel(self, text="font scale:")
+        self.font_scale_slider_label.grid(column=1, row=2)
+        self.font_scale_slider = customtkinter.CTkSlider(
+            self,
+            from_=0.1,
+            to=4,
+            number_of_steps=20,
+            width=width,
+            command=lambda x: self.set_and_update_canvas("font_scale", x)
+        )
+        self.font_scale_slider.set(self.font_scale)
+        self.font_scale_slider.grid(column=2, row=2)
+
+        self.segment_words_slider_label = customtkinter.CTkLabel(self, text="# words:")
+        self.segment_words_slider_label.grid(column=1, row=3)
+        self.segment_words_slider = customtkinter.CTkSlider(
+            self,
+            from_=1,
+            to=8,
+            number_of_steps=8,
+            width=width,
+            command=lambda x: self.set_and_update_canvas("words_per_segment", x)
+        )
+        self.segment_words_slider.set(self.words_per_segment)
+        self.segment_words_slider.grid(column=2, row=3)
+
+        self.outline_scale_slider_label = customtkinter.CTkLabel(self, text="outline scale:")
+        self.outline_scale_slider_label.grid(column=1, row=4)
+        self.outline_scale_slider = customtkinter.CTkSlider(
+            self,
+            from_=6,
+            to=30,
+            number_of_steps=24,
+            width=width,
+            command=lambda x: self.set_and_update_canvas("outline_scale", x)
+        )
+        self.outline_scale_slider.set(self.outline_scale)
+        self.outline_scale_slider.grid(column=2, row=4)
+
+        self.highlight_checkbox_label = customtkinter.CTkLabel(self, text="color current word:")
+        self.highlight_checkbox_label.grid(column=1, row=5)
+        self.strategy = customtkinter.StringVar(value="highlight")
+        self.highlight_checkbox = customtkinter.CTkCheckBox(self, text="", command=self.update_canvas,
+                                                            variable=self.strategy, onvalue="highlight", offvalue="off")
+        self.highlight_checkbox.select()
+        self.highlight_checkbox.grid(column=2, row=5, sticky="w", padx=5)
+
+        self.button_export_video = customtkinter.CTkButton(self, text="Export Video", command=self.export_video)
+        self.button_export_video.grid(column=1, row=6, padx=5, pady=24, columnspan=2)
+
         self.status_label = customtkinter.CTkLabel(self, text="status", fg_color="transparent", wraplength=width, justify=CENTER)
-        self.status_label.grid(column=1, row=2, sticky="n")
+        self.status_label.grid(column=1, row=7, columnspan=2, sticky="n")
         self.status_label.grid_remove()
         self.progress_bar = customtkinter.CTkProgressBar(self, orientation="horizontal", width=width)
         self.progress_bar.configure(mode="indeterminate", indeterminate_speed=1)
-        self.progress_bar.grid(column=1, row=3, sticky="sew", padx=5, pady=5)
+        self.progress_bar.grid(column=1, row=8, columnspan=2, sticky="n", padx=5, pady=5)
         self.progress_bar.grid_remove()
+
+        self.preview_canvas = Canvas(self, width=self.canvas_width, height=self.canvas_height)
+        self.preview_canvas.grid(column=4, row=0, rowspan=15, sticky="n", padx=5, pady=5)
+        self.update_canvas()
+
+    def update_canvas(self):
+        width, height = 1080, 1920
+        image = np.zeros((height, width, 3), np.uint8)
+        image[:,:,1] = 180
+        words = ["text" for _ in range(int(self.words_per_segment))]
+        if self.strategy.get() == "highlight":
+            image = add_words_with_outlines(
+                image,
+                words,
+                highlight_index=0,
+                font_scale=self.font_scale,
+                orient_y_percent=self.orient_y_percent,
+                outlines=[{'color': (0, 0, 0), 'thickness': int(self.outline_scale)}]
+            )
+        else:
+            text = " ".join(words)
+            image = add_text_with_outlines(
+                image,
+                text,
+                font_scale=self.font_scale,
+                orient_y_percent=self.orient_y_percent,
+                outlines=[{'color': (0, 0, 0), 'thickness': int(self.outline_scale)}]
+            )
+        image = cv2.resize(image, (self.canvas_width, self.canvas_height), interpolation=cv2.INTER_AREA)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        self.canvas_image = ImageTk.PhotoImage(image=Image.fromarray(image))
+        self.preview_canvas.create_image(0, 0, anchor="nw", image=self.canvas_image)
+
+    def set_and_update_canvas(self, attribute_name, value):
+        setattr(self, attribute_name, value)
+        self.update_canvas()
 
     def disable_buttons(self):
         self.button_load_video.configure(state="disabled")
@@ -78,7 +189,7 @@ class App(customtkinter.CTk):
         self.status_label.grid()
         self.progress_bar.grid()
         self.progress_bar.start()
-        TranscriptionWorker(self.transcription_queue, self.model, self.input_video, **self.transcribe_kwargs).start()
+        TranscriptionWorker(self.transcription_queue, self.model, self.input_video).start()
         self.after(500, self.poll_transcribe_results)
 
     def poll_transcribe_results(self):
@@ -106,42 +217,77 @@ class App(customtkinter.CTk):
             return
         try:
             words = string_to_words(self.textbox.get("0.0", "end").strip('\n'))
-        except Exception:  # TODO: fix broad exception clause
+        except Exception:
             self.status_label.configure(text="Error in transcript format")
             self.status_label.grid()
             return
         self.disable_buttons()
-        segments = create_segments(words, max_segment_words=5)
+        segments = create_segments(words, max_segment_words=int(self.words_per_segment))
         self.status_label.configure(text="Generating subtitled video...")
         self.status_label.grid()
         self.progress_bar.grid()
         self.progress_bar.start()
+        ExportWorker(
+            self.export_queue,
+            filename,
+            self.input_video,
+            segments,
+            font_scale=self.font_scale,
+            orient_y_percent=self.orient_y_percent,
+            outlines=[{'color': (0, 0, 0), 'thickness': int(self.outline_scale)}],
+            strategy=self.strategy.get()
+        ).start()
+        self.after(500, self.poll_export_results())
+
+    def poll_export_results(self):
+        try:
+            status = self.export_queue.get_nowait()
+            if status == "audio":
+                self.status_label.configure(text="Adding audio from original...")
+                self.after(500, self.poll_export_results)
+                return
+            self.progress_bar.stop()
+            self.progress_bar.grid_remove()
+            self.status_label.grid_remove()
+            self.enable_buttons()
+        except queue.Empty:
+            self.after(500, self.poll_export_results)
+
+
+class TranscriptionWorker(threading.Thread):
+    def __init__(self, transcribe_queue, model, filename):
+        self.queue = transcribe_queue
+        self.model = model
+        self.filename = filename
+        super().__init__()
+
+    def run(self):
+        words = transcribe_with_timestamps(self.model, self.filename)
+        self.queue.put(words)
+
+
+class ExportWorker(threading.Thread):
+    def __init__(self, export_queue, filename, input_video, segments, **subtitle_kwargs):
+        self.queue = export_queue
+        self.filename = filename
+        self.input_video = input_video
+        self.segments = segments
+        self.subtitle_kwargs = subtitle_kwargs
+        super().__init__()
+
+    def run(self):
         processed_video = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False)
         processed_video.close()
         create_subtitled_video(
             self.input_video,
             processed_video.name,
-            segments
+            self.segments,
+            **self.subtitle_kwargs
         )
-        add_movie_audio(self.input_video, processed_video.name, filename)
+        self.queue.put("audio")
+        add_movie_audio(self.input_video, processed_video.name, self.filename)
         os.remove(processed_video.name)
-        self.progress_bar.stop()
-        self.progress_bar.grid_remove()
-        self.status_label.grid_remove()
-        self.enable_buttons()
-
-
-class TranscriptionWorker(threading.Thread):
-    def __init__(self, transcribe_queue, model, filename, **transcribe_kwargs):
-        self.queue = transcribe_queue
-        self.model = model
-        self.filename = filename
-        self.transcribe_kwargs = transcribe_kwargs
-        super().__init__()
-
-    def run(self):
-        words = transcribe_with_timestamps(self.model, self.filename, **self.transcribe_kwargs)
-        self.queue.put(words)
+        self.queue.put("done")
 
 
 def run_app():
--- src/whisper_shorts_subs/subtitle.py
+++ src/whisper_shorts_subs/subtitle.py
@@ -37,7 +37,7 @@ def add_text_with_outlines(
     font_color=None,
     thickness=2,
     line_type=cv2.LINE_AA,
-    outlines = None,
+    outlines=None,
     inplace=True,
     orient_x_percent=0.5,
     orient_y_percent=0.5
@@ -50,7 +50,7 @@ def add_text_with_outlines(
         orient (None or Tuple[int, int]): The Location of the text relative to the top left corner.
         font (int): The cv2 font to use.
         font_scale (float): The size of the text.
-        font_color (Tuple[int, int, int]): Tuple containing ints 0-255 indicating rgb color.
+        font_color (Tuple[int, int, int]): Tuple containing ints 0-255 indicating bgr color.
         thickness (float): The thickness of the text.
         line_type (int): The cv2 line type.
         outlines (List[Dict]): Definitions of outline color and thicknesses. ex: [{'color': (0, 0, 0), 'thickness': 8}]
@@ -77,6 +77,74 @@ def add_text_with_outlines(
                 raise ValueError("Please provide outline options in the following format: [{'color': ..., 'thickness': ...}]")
             cv2.putText(result, text, orient, font, font_scale, outline['color'], outline['thickness'], line_type)
     result = cv2.putText(result, text, orient, font, font_scale, font_color, thickness, line_type)
+    return result
+
+
+def add_words_with_outlines(
+    image,
+    words,
+    highlight_index=None,
+    orient=None,
+    font=cv2.FONT_HERSHEY_TRIPLEX,
+    font_scale=2,
+    font_color=None,
+    highlight_color=None,
+    thickness=2,
+    line_type=cv2.LINE_AA,
+    outlines=None,
+    inplace=True,
+    orient_x_percent=0.5,
+    orient_y_percent=0.5
+):
+    """Add text with colored outlines to an image.
+
+    Args:
+        image (numpy.ndarray): The numpy array containing image data.
+        words (List[string]): The words to add to the image.
+        highlight_index (int): If provided, will highlight the nth word where n is highlight_index.
+        orient (None or Tuple[int, int]): The Location of the text relative to the top left corner.
+        font (int): The cv2 font to use.
+        font_scale (float): The size of the text.
+        font_color (Tuple[int, int, int]): Tuple containing ints 0-255 indicating bgr color.
+        highlight_color (Tuple[int, int, int]): If a word is highlighted, it will be displayed in this color.
+        thickness (float): The thickness of the text.
+        line_type (int): The cv2 line type.
+        outlines (List[Dict]): Definitions of outline color and thicknesses. ex: [{'color': (0, 0, 0), 'thickness': 8}]
+        inplace (bool): If True, will modify the image inplace.
+        orient_x_percent (float): If orient is None, the percentage of the screen from the left at which the text should
+            be centered horizontally.
+        orient_y_percent (float): If orient is None, the percentage of the screen from the top at which the text should
+            be centered vertically.
+
+    Returns:
+        (numpy.ndarray): The image data with text applied to it.
+    """
+    result = image
+    if not inplace:
+        result = np.copy(image)
+    if orient is None:
+        text_size = cv2.getTextSize(" ".join(words), font, font_scale, thickness)[0]
+        orient = (int((image.shape[1] - text_size[0])*orient_x_percent), int((image.shape[0] + text_size[1])*orient_y_percent))
+    word_widths = [cv2.getTextSize(word, font, font_scale, thickness)[0][0] for word in words]
+    space_size = 0
+    if len(word_widths) > 1:
+        space_size = (text_size[0] - sum(word_widths))/(len(word_widths) - 1)
+    font_color = font_color if font_color is not None else (255, 255, 255)
+    highlight_color = highlight_color if highlight_color is not None else (0, 0, 255)
+    outlines = outlines if outlines is not None else [{'color': (0, 0, 0), 'thickness': 8}]
+    if isinstance(outlines, list):
+        for outline in outlines:
+            if not isinstance(outline, dict) or 'color' not in outline or 'thickness' not in outline:
+                raise ValueError("Please provide outline options in the following format: [{'color': ..., 'thickness': ...}]")
+            current_x_offset = 0
+            for word, width in zip(words, word_widths):
+                cv2.putText(result, word, (int(orient[0] + current_x_offset), orient[1]), font, font_scale, outline['color'], outline['thickness'], line_type)
+                current_x_offset += width + space_size
+    current_x_offset = 0
+    for ix, (word, width) in enumerate(zip(words, word_widths)):
+        color = highlight_color if ix == highlight_index else font_color
+        cv2.putText(result, word, (int(orient[0] + current_x_offset), orient[1]), font, font_scale, color, thickness, line_type)
+        current_x_offset += width + space_size
     return result
 
 
@@ -109,9 +177,8 @@ def create_subtitled_video(
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(outfile, fourcc, fps, (frame_width, frame_height))
-    buf = np.empty((frame_height, frame_width, 3), np.dtype('uint8'))
     segment_index = 0
-    for frame in tqdm.tqdm(range(length)):
+    for _ in tqdm.tqdm(range(length)):
         ret, frame = cap.read()
         if not ret:
             break
@@ -125,6 +192,30 @@ def create_subtitled_video(
                 out.write(frame)
                 continue
         if timestamp < segments[segment_index][0].start:
+            out.write(frame)
+            continue
+        if strategy == "highlight":
+            words = []
+            highlight_index = None
+            for ix, word in enumerate(segments[segment_index]):
+                words.append(word.word.strip())
+                if (timestamp > word.start) and (timestamp <= word.end):
+                    highlight_index = ix
+            frame = add_words_with_outlines(
+                frame,
+                words,
+                highlight_index=highlight_index,
+                orient=orient,
+                font=font,
+                font_scale=font_scale,
+                font_color=font_color,
+                thickness=thickness,
+                line_type=line_type,
+                outlines=outlines,
+                inplace=inplace,
+                orient_x_percent=orient_x_percent,
+                orient_y_percent=orient_y_percent
+            )
             out.write(frame)
             continue
         if strategy == "type":
